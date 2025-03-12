aa2758dea03b1ff96ff292cbc6ea8097ec0a28a6
don <eleusizdaniyar777@gmail.com>
2025-03-13 02:57:34 +0500

    fix

---
+++ account-service/src/main/java/org/example/accountservice/feign/FeignClientInterceptor.java
@@ -0,0 +1,26 @@
+package org.example.accountservice.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private final HttpServletRequest request;
+
+    @Autowired
+    public FeignClientInterceptor(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String token = request.getHeader("Authorization");
+        if (token != null) {
+            requestTemplate.header("Authorization", token);
+        }
+    }
+}
